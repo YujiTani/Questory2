85e0478cb88f2ef9850a61669d9621d45b68d2b1
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-04-25 13:11:48 +0900

    date用のvalue-objectsを追加

---
+++ src/domain/common/date.value-objects.ts
@@ -0,0 +1,61 @@
+export abstract class DateValueObject {
+  constructor(private readonly value: Date) {
+    this.value = value;
+    this.validate();
+  }
+
+  getValue(): Date {
+    return new Date(this.value);
+  }
+
+  equals(other: DateValueObject): boolean {
+    if (!(other instanceof this.constructor)) {
+      return false;
+    }
+    return this.value.getTime() === other.value.getTime();
+  }
+
+  toISOString(): string {
+    return this.value.toISOString();
+  }
+
+  toMilliseconds(): number {
+    return this.value.getTime();
+  }
+
+  protected validate(): void {
+    if (!(this.value instanceof Date) || Number.isNaN(this.value.getTime())) {
+      throw new Error(`${this.constructor.name} must be a valid date`);
+    }
+  }
+}
+
+export class CreatedAt extends DateValueObject {
+  private constructor(value: Date) {
+    super(value);
+  }
+
+  static create(date: Date = new Date()): CreatedAt {
+    return new CreatedAt(date);
+  }
+}
+
+export class UpdatedAt extends DateValueObject {
+  private constructor(value: Date) {
+    super(value);
+  }
+
+  static create(date: Date = new Date()): UpdatedAt {
+    return new UpdatedAt(date);
+  }
+}
+
+export class DeletedAt extends DateValueObject {
+  private constructor(value: Date) {
+    super(value);
+  }
+
+  static create(date: Date = new Date()): DeletedAt {
+    return new DeletedAt(date);
+  }
+}
