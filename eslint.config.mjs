04d860f8da5eaf049d3f583bf09f7ffd65d6a4a7
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-04-27 21:29:11 +0900

    設定を変更

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,6 @@ import tsParser from "@typescript-eslint/parser";
 import _import from "eslint-plugin-import";
 import importAlias from "eslint-plugin-import-alias";
 import unusedImports from "eslint-plugin-unused-imports";
-import { warn } from "node:console";
 
 // MEMO: 直したいけど直し方がわからない、一応動くのでこのままにしておく
 const __filename = fileURLToPath(import.meta.url);
@@ -70,15 +69,40 @@ export default [
     },
 
     rules: {
+      // 一般的なDDD用の緩和ルール
       "consistent-return": "off",
       "no-underscore-dangle": "off",
+      "no-useless-constructor": "off",
+      "@typescript-eslint/no-useless-constructor": "error",
+      "no-empty-function": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "class-methods-use-this": "off",
+      "max-classes-per-file": "off",
+      
+      // プロパティの命名規則の緩和（DDDで_privatePropsが使いやすくなる）
+      "camelcase": "off",
+      
+      // 未使用変数チェックの緩和
+      // TODO: 一旦警告にするが、構築完了後に調整が必要
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          args: "none",
+          caughtErrors: "all",
+        },
+      ],
 
+      // インポート関連
       "import/extensions": "off",
       "import/prefer-default-export": "off",
       "unused-imports/no-unused-imports": "error",
 
       "import/order": [
-        "error",
+        "warn",
         {
           groups: [
             "builtin",
@@ -99,16 +123,14 @@ export default [
       ],
 
       "@typescript-eslint/consistent-type-imports": [
-        "error",
-        {
-          prefer: "type-imports",
-        },
+        "warn",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
       ],
 
       "import-alias/import-alias": [
-        "error",
+        "warn",
         {
-          aliases: [{ alias: "@", matcher: "./src" }],
+          "relativeDepth": 0
         },
       ],
 
@@ -118,17 +140,15 @@ export default [
           patterns: ["./*", "../*"],
         },
       ],
-      "max-classes-per-file": "off",
     },
   },
+  // テスト用の設定
   {
-    files: ["**/*.value-objects.ts"],
+    files: ["**/*.test.ts", "**/*.spec.ts"],
     rules: {
-      "@typescript-eslint/no-unused-private-class-members": "off",
+      "no-unused-expressions": "off",
+      "max-classes-per-file": "off",
       "no-useless-constructor": "off",
-      "@typescript-eslint/no-useless-constructor": "error",
-      "no-empty-function": "off",
-      "@typescript-eslint/no-empty-function": "error",
     },
   },
 ];
