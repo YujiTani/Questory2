b2646d7995ec6c1321d5ddc0fa19bb84aa3fd3f3
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-05 10:48:19 +0900

    add; createmethods

---
+++ src/domain/question-collection/id.value-objects.ts
@@ -0,0 +1,9 @@
+import { randomUUIDv7 } from "bun";
+
+import { Id } from "@/domain/common/id.value-objects";
+
+export class QuestionCollectionId extends Id {
+  static create() {
+    return new QuestionCollectionId(randomUUIDv7());
+  }
+}
