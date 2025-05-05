e8e6471d8b522eb8c4323b3c76e14e685064985c
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-05 22:31:25 +0900

    テストを修正

--- src/tests/application/use-cases/question/create-question.usecase.test.ts
+++ src/tests/application/use-cases/question/create-question.usecase.test.ts
@@ -119,7 +119,7 @@ describe("create-question.usecase.tsのテスト", () => {
     expect(result.toDTO()).toEqual(expect.objectContaining(expected));
   })
 
-  test("カテゴリー:TYPESCRIPT、タイプ:SELECTで問題を作成できる", async () => {
+  test("カテゴリー:TYPESCRIPT、タイプ:SORTで問題を作成できる", async () => {
     const date = new Date("2025-01-01T12:00:00Z")
     setSystemTime(date);
   
@@ -132,7 +132,7 @@ describe("create-question.usecase.tsのテスト", () => {
         "TypeScriptはCSSフレームワークの一種です。"
       ],
       "TypeScriptはJavaScriptのスーパーセットで、JavaScriptに静的型付けのシステムを追加した言語です。すべての有効なJavaScriptコードは有効なTypeScriptコードでもあり、型注釈はオプションです。",
-      "SELECT",
+      "SORT",
       "ACTIVE",
       "TYPESCRIPT",
     )
@@ -152,7 +152,7 @@ describe("create-question.usecase.tsのテスト", () => {
         "TypeScriptはCSSフレームワークの一種です。"
       ],
       explanation: "TypeScriptはJavaScriptのスーパーセットで、JavaScriptに静的型付けのシステムを追加した言語です。すべての有効なJavaScriptコードは有効なTypeScriptコードでもあり、型注釈はオプションです。",
-      type: "SELECT",
+      type: "SORT",
       state: "ACTIVE",
       category: "TYPESCRIPT",
       }
@@ -168,7 +168,7 @@ describe("create-question.usecase.tsのテスト", () => {
       ],
       explanation:
         "SELECT文では * を使用することで全てのカラムを選択できます。",
-      type: "SELECT",
+      type: "SORT",
       state: "ACTIVE",
       category: "TYPESCRIPT",
       createdAt: "2025-01-01T12:00:00.000Z",
@@ -178,5 +178,62 @@ describe("create-question.usecase.tsのテスト", () => {
 
     expect(result.toDTO()).toEqual(expect.objectContaining(expected));
   })
-  test("カテゴリー:HTTP、タイプ:MULTIPLE_CHOICEで問題を作成できる", () => {})
+  test("カテゴリー:HTTP、タイプ:MULTIPLE_CHOICEで問題を作成できる",  async () => {
+    const date = new Date("2025-01-01T12:00:00Z")
+    setSystemTime(date);
+  
+    const questionEntity = QuestionEntity.create(
+      "HTTPステータスコードのうち、クライアントエラーを示すものはどれですか？（複数選択）",
+      ["404 Not Found", "400 Bad Request"],
+      [
+        "200 OK",
+        "301 Moved Permanently",
+        "500 Internal Server Error"
+      ],
+      "HTTPステータスコードは3桁の数字で、最初の数字によって5つのクラスに分類されます。「4xx」で始まるコードはクライアントエラーを示し、「404 Not Found」はリソースが見つからない場合、「400 Bad Request」はリクエストの構文が不正な場合に返されます。一方、「2xx」は成功、「3xx」はリダイレクト、「5xx」はサーバーエラーを示します。",
+      "MULTIPLE_CHOICE",
+      "ACTIVE",
+      "HTTP",
+    )
+    const mockQuestionReositoy = {
+      save: mock(async () => questionEntity)
+    }
+
+    const createQuestionUseCase = new CreateQuestionUseCase(mockQuestionReositoy)
+
+    const result = await createQuestionUseCase.execute(
+      {
+      text: "HTTPステータスコードのうち、クライアントエラーを示すものはどれですか？（複数選択）",
+      correctAnswer: ["404 Not Found", "400 Bad Request"],
+      alternativeAnswers: [
+        "200 OK",
+        "301 Moved Permanently",
+        "500 Internal Server Error"
+      ],
+      explanation: "HTTPステータスコードは3桁の数字で、最初の数字によって5つのクラスに分類されます。「4xx」で始まるコードはクライアントエラーを示し、「404 Not Found」はリソースが見つからない場合、「400 Bad Request」はリクエストの構文が不正な場合に返されます。一方、「2xx」は成功、「3xx」はリダイレクト、「5xx」はサーバーエラーを示します。",
+      type: "MULTIPLE_CHOICE",
+      state: "ACTIVE",
+      category: "HTTP",
+      }
+    );
+
+    const expected = {
+      text: "HTTPステータスコードのうち、クライアントエラーを示すものはどれですか？（複数選択）",
+      correctAnswer: ["404 Not Found", "400 Bad Request"],
+      alternativeAnswers: [
+        "200 OK",
+        "301 Moved Permanently",
+        "500 Internal Server Error"
+      ],
+      explanation: "HTTPステータスコードは3桁の数字で、最初の数字によって5つのクラスに分類されます。「4xx」で始まるコードはクライアントエラーを示し、「404 Not Found」はリソースが見つからない場合、「400 Bad Request」はリクエストの構文が不正な場合に返されます。一方、「2xx」は成功、「3xx」はリダイレクト、「5xx」はサーバーエラーを示します。",
+      type: "MULTIPLE_CHOICE",
+      state: "ACTIVE",
+      category: "HTTP",
+      createdAt: "2025-01-01T12:00:00.000Z",
+      updatedAt: "2025-01-01T12:00:00.000Z",
+      deletedAt: null,
+    };
+
+    expect(result.toDTO()).toEqual(expect.objectContaining(expected));
+  })
 });
