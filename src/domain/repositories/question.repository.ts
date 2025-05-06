06f5d92f74e5480d6d4d153856542df896416585
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-07 00:24:09 +0900

    findByIdメソッドをinterface追加

--- src/domain/repositories/question.repository.ts
+++ src/domain/repositories/question.repository.ts
@@ -1,7 +1,9 @@
 import type { QuestionEntity } from "@/domain/entities/question";
+import type { QuestionId } from "@/domain/value-objects/question/id.value-objects";
 
 /** 問題リポジトリー */
 export interface QuestionRepository {
   /** 問題を保存 */
-  save(question: QuestionEntity): Promise<QuestionEntity>;
+  save(question: QuestionEntity): Promise<QuestionEntity>
+  findById(id: QuestionId): Promise<QuestionEntity>
 }
