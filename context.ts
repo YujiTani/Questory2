e487e974927f6931d814ab085be263c307037ca1
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-06 23:20:27 +0900

    prismaでmockするためのライブラリをインストール

---
+++ context.ts
@@ -0,0 +1,14 @@
+import { type PrismaClient } from '@prisma/client'
+import { mockDeep, type DeepMockProxy } from 'jest-mock-extended'
+
+export type Context = {
+  prisma: PrismaClient
+}
+
+export type MockContext = {
+  prisma: DeepMockProxy<PrismaClient>
+}
+
+export const createMockContext = (): MockContext => ({
+    prisma: mockDeep<PrismaClient>(),
+  })
