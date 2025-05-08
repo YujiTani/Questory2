ac3a02579c5fcc1ad19d5b42411cdfca5080f550
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-08 09:36:29 +0900

    repositoryテスト修正

--- src/tests/infrastructure/repositories/sql-question.repository.test.ts
+++ src/tests/infrastructure/repositories/sql-question.repository.test.ts
@@ -6,7 +6,7 @@ import type { PrismaClient } from "@prisma/client";
 import { SQLQuestionRepository } from "@/infrastructure/repositories/sql-question.repository";
 import { TestQuestionFactory } from "@/tests/factories/test-question.factory";
 
-describe("sql-question.repository.tsのテスト", () => {
+describe.only("sql-question.repository.tsのテスト", () => {
   // グローバルに扱う値の定義
   let prismaMock: DeepMockProxy<PrismaClient>;
   let sqlQuestionRepositoy: SQLQuestionRepository;
@@ -18,27 +18,29 @@ describe("sql-question.repository.tsのテスト", () => {
 
   test("saveしたものがfindByIdで取得できること", async () => {
     // given (前提条件)：操作を実行する前の状態
-    const question = TestQuestionFactory.create();
-
+    const question = TestQuestionFactory.create()
+    
     // when (操作): 操作
     await sqlQuestionRepositoy.save(question);
-    const foundQuestion =  await sqlQuestionRepositoy.findById(question.getId)
+    const foundQuestion =  (await sqlQuestionRepositoy.findById(question.getId))
+
 
     // then (結果) : 操作した結果
-    // DBへの保存が成功したことをチェックしたい
+    // 実際にはentityで返すが、テストでは比較のためにDTOに変換している
+    const questionDTO = question.toDTO()
+    const foundQuestionDTO = foundQuestion.toDTO()
 
-    // FIXME: ここに返すのはEntityなのか？DB登録に使うのはDTOのはず？DTOからentityに戻すにはreconstructメソッドを使うように設計したい
-    expect(foundQuestion.id).toBe(question.getId);
-    expect(foundQuestion.text).toBe(question.text);
-    expect(foundQuestion.correctAnswer).toBe(question.correctAnswer);
-    expect(foundQuestion.alternativeAnswers).toBe(question.alternativeAnswers);
-    expect(foundQuestion.explanation).toBe(question.explanation);
-    expect(foundQuestion.type).toBe(question.type);
-    expect(foundQuestion.state).toBe(question.state);
-    expect(foundQuestion.category).toBe(question.category);
-    expect(foundQuestion.createdAt).toBe(question.createdAt);
-    expect(foundQuestion.updatedAt).toBe(question.updatedAt);
-    expect(foundQuestion.deletedAt).toBe(question.deletedAt);
+    expect(foundQuestionDTO.id).toBe(questionDTO.id);
+    expect(foundQuestionDTO.text).toBe(questionDTO.text);
+    expect(foundQuestionDTO.correctAnswer).toBe(questionDTO.correctAnswer);
+    expect(foundQuestionDTO.category).toBe(questionDTO.category);
+    expect(foundQuestionDTO.type).toBe(questionDTO.type);
+    expect(foundQuestionDTO.alternativeAnswers).toBe(questionDTO.alternativeAnswers);
+    expect(foundQuestionDTO.explanation).toBe(questionDTO.explanation);
+    expect(foundQuestionDTO.state).toBe(questionDTO.state);
+    expect(foundQuestionDTO.createdAt).toBe(questionDTO.createdAt);
+    expect(foundQuestionDTO.updatedAt).toBe(questionDTO.updatedAt);
+    expect(foundQuestionDTO.deletedAt).toBe(questionDTO.deletedAt);
   });
 
   test.skip("****", () => {
