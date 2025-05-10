ca5f44be66db5aa598a33f9a03812bdd2f05fea5
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-10 21:06:58 +0900

    add; entityへの変換処理

--- src/infrastructure/repositories/sql-question.repository.ts
+++ src/infrastructure/repositories/sql-question.repository.ts
@@ -1,10 +1,10 @@
 import { type PrismaClient } from "@prisma/client";
 
 import type { QuestionRepository } from "@/domain/repositories/question.repository";
-import type { QuestionId } from "@/domain/value-objects/question/id.value-objects";
 
-import { QuestionEntity } from "@/domain/entities/question";
-import type { QuestionCategory, QuestionState, QuestionType } from "@/domain/entities/question"
+import { type QuestionEntity, questionTypes, type QuestionCategory, type QuestionState, type QuestionType, questionCategory, questionState } from "@/domain/entities/question"
+import { type QuestionId } from "@/domain/value-objects/question/id.value-objects";
+
 
 export class SQLQuestionRepository implements QuestionRepository {
   constructor(private prisma: PrismaClient) {}
@@ -42,21 +42,134 @@ export class SQLQuestionRepository implements QuestionRepository {
   }
 
   /**
+   * モデルからエンティティーに変換
+   */
+  toEntity(model: {
+    id: string,
+    text: string,
+    correctAnswer: string[],
+    alternativeAnswers: string[],
+    explanation: string,
+    type: number,
+    state: number,
+    category: number,
+    createdAt: BigInt,
+    updatedAt: BigInt,
+    deletedAt: BigInt | null,
+  }) {
+    return {
+      id: model.id,
+      text: model.text,
+      correctAnswer: model.correctAnswer,
+      alternativeAnswers: model.alternativeAnswers,
+      explanation: model.explanation,
+      type: this.toEntity$type(model.type), 
+      state: this.toEntity$state(model.state), 
+      category: this.toEntity$category(model.category),
+      createdAt: new Date(Number(model.createdAt)),
+      updatedAt:  new Date(Number(model.updatedAt)),
+      deletedAt: this.toEntity$deletedAt(model.deletedAt)
+    }
+  }
+
+  /**
+   * モデルをエンティティーに変換; 問題の種類
+   * @param type number 問題の種類
+   * @return QuestionType 数値をQuestionTypeに変換
+   */
+  toEntity$type(type: number): QuestionType {
+    switch (type)
+    {
+      case 10:
+        return questionTypes.select
+      
+      case 20:
+        return questionTypes.multiple_choice
+
+      case 30:
+        return questionTypes.sort
+      
+      default:
+        throw new Error(`${__filename}, 問題が想定外の値になっています ${type}`)
+    }
+  }
+ 
+  /**
+   * モデルをエンティティーに変換; 問題の状態
+   * @param type number 問題の状態
+   * @return QuestionType 数値をQuestionTypeに変換
+   */
+  toEntity$state(state: number): QuestionState {
+    switch(state)
+    {
+      case 10:
+        return questionState.active 
+
+      case 20:
+        return questionState.retry
+
+      case 30:
+        return questionState.last_attempt
+
+      case 40:
+        return questionState.skipped
+
+      case 50:
+        return questionState.difficult
+      
+    default:
+      throw new Error(`${__filename}, 問題が想定外の状態になっています ${state}`)
+    }
+  }
+
+  /**
+   * モデルをエンティティーに変換; 問題の状態
+   * @param type number 問題の状態
+   * @return QuestionType 数値をQuestionTypeに変換
+   */
+  toEntity$category(category: number): QuestionCategory {
+    switch(category)
+    {
+      case 10:
+        return questionCategory.http
+
+      case 20:
+        return questionCategory.sql
+
+      case 30:
+        return questionCategory.typescript
+
+    default:
+      throw new Error(`${__filename}, 問題のカテゴリーが想定外の値になっています ${category}`)
+    }
+  }
+
+  /**
+   * モデルをエンティティーに変換; 論理削除フラグ
+   * @param deletedAt BigInt | null
+   * @return Date | null
+   */
+  toEntity$deletedAt(deletedAt: BigInt | null): Date | null {
+    return deletedAt ? new Date(Number(deletedAt)) : null
+  }
+
+  /**
    * エンティティーをモデルに変換
    */
   toModel(entity: QuestionEntity) {
-    id: entity.id,
-    uuid: entity.uuid,
-    text: entity.text,
-    correctAnswer: entity.correctAnswer,
-    alternativeAnswers: entity.alternativeAnswers,
-    explanation: entity.explanation,
-    type: this.toModel$type(entity.type),
-    state: this.toModel$state(entity.state),
-    category: this.toModel$category(entity.category),
-    createdAt: BigInt(entity.createdAt.getTime),
-    updatedAt: BigInt(entity.updatedAt.getTime),
-    deletedAt: entity.getDeletedAt ? BigInt(entity.getDeletedAt?.getTime): null,
+    return {
+      uuid: entity.id,
+      text: entity.text,
+      correctAnswer: entity.correctAnswer,
+      alternativeAnswers: entity.alternativeAnswers,
+      explanation: entity.explanation,
+      type: this.toModel$type(entity.type),
+      state: this.toModel$state(entity.state),
+      category: this.toModel$category(entity.category),
+      createdAt: BigInt(entity.createdAt.getTime()),
+      updatedAt: BigInt(entity.updatedAt.getTime()),
+      deletedAt: entity.deletedAt ? BigInt(entity.deletedAt.getTime()): null,
+    }
   }
 
 /**
